398929909639db9b589f6f4ed1db4719547e606d
Vilho Kn <73425056+VilhoKn@users.noreply.github.com>
2022-03-06 23:11:13 +0000

    Made skeleton for the file

--- src/code/main.py
+++ src/code/main.py
@@ -3,3 +3,14 @@ from time import sleep
 import schedule
 from info import username, password
 
+bot = Bot()
+bot.login(username=username, password=password)
+
+def post_pick():
+  pass
+
+schedule.every().day.at("19:00").do(post_pick)
+
+while True:
+  schedule.run_pending()
+  sleep(1)
