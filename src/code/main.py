58fcd0b27488072c3d96e5173f1ba22f3ea9e7bd
Vilho Kn <73425056+VilhoKn@users.noreply.github.com>
2022-03-07 18:36:59 +0000

    Updated test

--- src/code/main.py
+++ src/code/main.py
@@ -1,7 +1,8 @@
 from instabot import Bot
 from time import sleep
 import schedule
-from info import username, password
+import tweepy
+from info import *
 
 bot = Bot()
 bot.login(username=username, password=password)
@@ -12,16 +13,33 @@ def post_pic():
   with open("../files/day.txt", "w") as f:
     f.write(str(day))
 
-  caption = f"Day {day} #spoonkid #dinkbot #blazed #monke #monkeclothing #funeemonkee #pietown #rust #majorbagalert #goomba #twitch #youtube"
+  caption = f"Day {day}"
+  ig_caption = caption + " #spoonkid #dinkbot #blazed #monke #monkeclothing #funeemonkee #pietown #rust #majorbagalert #goomba #twitch #youtube"
   
-  image_path = "../files/monke.jpg"
+  img_path = "../files/monke.jpg"
 
-  bot.upload_photo(image_path, caption=caption)
-  print("Tried to upload pic")
-  
+  print(f"Posting day {day}...")
+  post_instagram(bot, img_path, ig_caption)
+  post_twitter(img_path, caption)
+
+
+def post_instagram(bot, img_path, caption):
+  bot.upload_photo(img_path, caption=caption)
+  print("Tried to upload pic to instagram")
+
   if bot.api.last_response.status_code != 200:
-    print(f"Error: {bot.api.last_response}")
+    print(f"Instagram Error: {bot.api.last_response}")
   else:
-    print("Pic was uploaded successfully")
+    print("Pic was uploaded successfully to instagram" )
+
+
+def post_twitter(img_path, caption):
+  authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
+  authenticator.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(authenticator, wait_on_rate_limit=True)
+
+  api.upload_with_image(img_path, status=caption)
+  print("Tried to upload pic to twitter")
 
 post_pic()
