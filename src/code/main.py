2f662cd35cd68869372bbd949144e7894e7afeb5
Vilho Kn <73425056+VilhoKn@users.noreply.github.com>
2022-03-07 10:57:23 +0000

    Made the function

--- src/code/main.py
+++ src/code/main.py
@@ -7,7 +7,23 @@ bot = Bot()
 bot.login(username=username, password=password)
 
 def post_pick():
-  pass
+  with open("../files/day.txt", "r") as f:
+    day = int(f.read()) + 1
+  with open("../files/day.txt", "w") as f:
+    f.write(str(day))
+
+  caption = f"Day {day} #spoonkid #dinkbot #blazed #monke #monkeclothing #funeemonkee #pietown #rust #majorbagalert #goomba #twitch #youtube"
+  
+  image_path = "../files/monke.jpg"
+
+  bot.upload_photo(image_path, caption=caption)
+  print("Tried to upload pic")
+  
+  if bot.api.last_response.status_code != 200:
+    print(f"Error: {bot.api.last_response}")
+  else:
+    print("Pic was uploaded successfully")
+    
 
 schedule.every().day.at("19:00").do(post_pick)
 
