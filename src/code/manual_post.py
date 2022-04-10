3401614bb7aa1e34b9cb2fe6440aff3acab186a0
unknown <knuutila.vilho@gmail.com>
2022-04-10 17:48:56 +0300

    updated manual post

--- src/code/manual_post.py
+++ src/code/manual_post.py
@@ -1,3 +1,30 @@
-from main import post_pic
+from main import post_instagram, post_twitter
+import sys
+import os
 
-post_pic()
+def post_pic(same_day, platforms):
+	if os.path.isfile("config/daily_monke_pic_uuid_and_cookie.json"):
+		os.remove("config/daily_monke_pic_uuid_and_cookie.json")
+
+	adder = 0 if same_day else 1
+		
+	with open("../files/day.txt", "r") as f:
+		day = int(f.read()) + adder
+	with open("../files/day.txt", "w") as f:
+		f.write(str(day))
+
+	caption = f"Day {day}"
+	ig_caption = caption + " #spoonkid #dinkbot #blazed #monke #monkeclothing #funeemonkee #pietown #rust #majorbagalert #goomba #twitch #youtube"
+	
+	img_path = "../files/monke.jpg"
+
+	print(f"Posting day {day}...")
+	if platforms == "instagram" or platforms == "both":
+		post_instagram(img_path, ig_caption)
+	if platforms == "twitter" or platforms == "both":
+		post_twitter(img_path, caption)
+
+same_day = sys.argv[1]
+platforms = sys.argv[2]
+
+post_pic(same_day, platforms)
